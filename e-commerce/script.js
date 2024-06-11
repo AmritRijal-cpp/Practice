dad17d9176e6eb2c9013e51e77af1d29057062e0
AmritRijal-cpp <rijalamrit987@gmail.com>
2024-06-11 15:11:08 +0545

    updated

--- e-commerce/script.js
+++ e-commerce/script.js
@@ -17,6 +17,19 @@ let list = [
 
 ]
 
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".navMenu");
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+})
+
+document.querySelectorAll(".item").forEach(n => n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}))
+
 let currentIndex = 1;
 
 function next() {
